773d61170768689d510ebb61be0a424367bb4eef
Cblpom <matveyburak@mail.ru>
2018-10-28 16:35:27 +0300

    Task24

---
+++ src/main/java/com/epam/homework/task25/Task25Impl.java
@@ -0,0 +1,17 @@
+package com.epam.homework.task25;
+
+public class Task25Impl implements Task25 {
+    @Override
+    public boolean isNormalBrackets(String string) {
+        int sum=0;
+        for (int i = 0; i<string.length(); i++){
+            if (string.charAt(i)== '('){sum++;}
+            else {sum--;}
+            if (sum <0 ) {return false;}
+        }
+        return sum == 0;
+    }
+
+
+
+}
